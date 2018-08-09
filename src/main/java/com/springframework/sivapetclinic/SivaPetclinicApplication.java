811d1e44b65bfb1da848bf9f0a4a71bfa67f471e
Siva Mattapally <Siva.Mattapally@macys.com>
2018-08-09 13:05:59 -0400

    Initial Project Add

---
+++ src/main/java/com/springframework/sivapetclinic/SivaPetclinicApplication.java
@@ -0,0 +1,12 @@
+package com.springframework.sivapetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SivaPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SivaPetclinicApplication.class, args);
+    }
+}
