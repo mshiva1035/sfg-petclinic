3222e4d75fa32f6e7b5bc9021b456492b3140305
Siva Mattapally <Siva.Mattapally@macys.com>
2018-08-14 19:02:42 -0300

    Update Map impl to manage settings of ID property. Closes #23

--- pet-clinic-web/src/main/java/com/springframework/sivapetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springframework/sivapetclinic/bootstrap/DataLoader.java
@@ -23,25 +23,21 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner = new Owner();
-        owner.setId(1L);
         owner.setFirstName("Michael");
         owner.setLastName("Weston");
         ownerService.save(owner);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Siva");
         owner2.setLastName("Mattapally");
         ownerService.save(owner2);
 
         Vet vet = new Vet();
-        vet.setId(1L);
         vet.setFirstName("Sam");
         vet.setLastName("Axz");
         vetService.save(vet);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Jazzy");
         vetService.save(vet2);
