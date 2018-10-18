6e145e031836cbe06a71983e2cb48d003d5c7ea5
Siva Mattapally <Siva.Mattapally@macys.com>
2018-10-18 12:29:13 -0300

    Added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/com/springframework/sivapetclinic/services/PetTypeService.java
@@ -0,0 +1,8 @@
+package com.springframework.sivapetclinic.services;
+
+import com.springframework.sivapetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+
+}
---
+++ pet-clinic-data/src/main/java/com/springframework/sivapetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package com.springframework.sivapetclinic.services.map;
+
+import com.springframework.sivapetclinic.model.PetType;
+import com.springframework.sivapetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteBydId(aLong);
+    }
+}
