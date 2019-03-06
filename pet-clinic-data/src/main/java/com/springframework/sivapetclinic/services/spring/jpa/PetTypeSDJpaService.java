bc960231af7b1ff66e5ca0f69c7ff554acc9a5bd
Siva Mattapally <Siva.Mattapally@macys.com>
2019-03-06 18:35:37 -0300

    Pet Type JPA Service . Closes #42

---
+++ pet-clinic-data/src/main/java/com/springframework/sivapetclinic/services/spring/jpa/PetTypeSDJpaService.java
@@ -0,0 +1,46 @@
+package com.springframework.sivapetclinic.services.spring.jpa;
+
+import com.springframework.sivapetclinic.model.PetType;
+import com.springframework.sivapetclinic.repositories.PetTypeRepository;
+import com.springframework.sivapetclinic.services.PetTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetTypeSDJpaService implements PetTypeService {
+
+    @Autowired
+    private PetTypeRepository petTypeRepository;
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> pets = new HashSet<>();
+        petTypeRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+}
